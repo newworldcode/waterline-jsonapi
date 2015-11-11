363dbaaaf6f4aa64b66789efbb26e2aaa4907220
davemackintosh <me@davemackintosh.co.uk>
2015-11-11 09:40:15 +0000

    Fixed bad naming convention
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/all.js
+++ tests/all.js
@@ -2,7 +2,7 @@
 
 // Get our tools.
 var tape = require("tape")
-var waterline_jsonapi = require("../index")
+var JSONAPIModel = require("../index")
 var get_ontology = require("./waterline")
 
 // The test payload.
@@ -12,14 +12,14 @@ get_ontology(function (ontology) {
   tape("Test simple payload", function(test) {
     test.plan(1)
     test.doesNotThrow(function() {
-      waterline_jsonapi.new_from_values(payloads.simple, ontology.collections.user)
+      JSONAPIModel.new_from_values(payloads.simple, ontology.collections.user)
     }, "Does not throw when creating simple payload.")
   })
 
   tape("Test complex payload", function(test) {
     test.plan(1)
     test.doesNotThrow(function() {
-      waterline_jsonapi.new_from_values(payloads.complex, ontology.collections.user)
+      JSONAPIModel.new_from_values(payloads.complex, ontology.collections.user)
     }, "Does not throw when creating complex payload.")
   })
 })
